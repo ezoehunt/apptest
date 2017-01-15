1df6cc78fe728ca6ee541e106fe502c86f9c2972
ezoehunt <ezoehunt@gmail.com>
2017-01-15 14:52:54 -0800

    remove rails admin

--- Gemfile
+++ Gemfile
@@ -23,6 +23,10 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.2'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
+# Authentication
+gem 'omniauth', '~> 1.3.1'
+gem 'omniauth-auth0', '~> 1.4.1'
+# Admin
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,19 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-  root 'welcome#index'
+
+# routes regular users
+#scope :app do
+
+#end
+
+# routes for admin users
+#namespace :admin do
+
+#end
+
+get 'welcome/index'
+root 'welcome#index'
+
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
