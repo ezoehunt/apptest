f91d18d5d9188f72d989c7f5f5f642fe472e9a7c
ezoehunt <ezoehunt@gmail.com>
2017-01-15 11:26:59 -0800

    assets pipeline

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,7 +41,7 @@ Rails.application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
     # Setting to 'false' concatentates all js files
-  config.assets.debug = true
+  config.assets.debug = false
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
@@ -52,4 +52,12 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Precompile assets on local
+  config.assets.prefix = "/dev-assets"
+
+  # Compress JavaScripts and CSS.
+  config.assets.js_compressor = :uglifier
+  config.assets.css_compressor = :sass
+  
 end
