711acacde1ab46e77a592189a4aae98ae16fc605
ezoehunt <ezoehunt@gmail.com>
2017-01-16 11:08:02 -0800

    clean up for railsadmin

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
-  devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
+mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+
 # routes regular users
 #scope :app do
 
