4cbf3dd5280942439226d2dd80c38b68b267603d
ezoehunt <ezoehunt@gmail.com>
2017-01-15 20:35:31 -0800

    rename db

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_apptest_session'
+Rails.application.config.session_store :cookie_store, key: '_bestofpotus_session'
